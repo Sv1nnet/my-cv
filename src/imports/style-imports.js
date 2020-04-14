9aeef3b324e969a93a35635daf63b902a731c410
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-04-14 18:30:13 +0300

    Desctop version done

--- src/imports/style-imports.js
+++ src/imports/style-imports.js
@@ -3,15 +3,37 @@ import 'normalize.css';
 import '@blocks/body/body.scss';
 import '@blocks/body/__inner/body__inner.scss';
 import '@blocks/body/__header/body__header.scss';
-import '@blocks/body/__contacts/body__contacts.scss';
 import '@blocks/body/__projects/body__projects.scss';
 
+import '@blocks/header/header.scss';
+import '@blocks/header/__author/header__author.scss';
 import '@blocks/header/__full-name/header__full-name.scss';
+import '@blocks/header/__contacts/header__contacts.scss';
 import '@blocks/header/__profession/header__profession.scss';
 
-import '@blocks/contacts/__phone/contacts__phone.scss';
+import '@blocks/main/main.scss';
+import '@blocks/main/__projects/main__projects.scss';
+import '@blocks/main/__skills/main__skills.scss';
+import '@blocks/main/__section-title/main__section-title.scss';
+import '@blocks/main/__experience/main__experience.scss';
+
+import '@blocks/projects/projects.scss';
+import '@blocks/projects/__project-wrapper/projects__project-wrapper.scss';
+import '@blocks/projects/__title/projects__title.scss';
+import '@blocks/projects/__title-explanation/projects__title-explanation.scss';
+
+import '@blocks/section-title/section-title.scss';
 
 import '@blocks/contact-link/contact-link.scss';
 
 import '@blocks/text/text.scss';
 import '@blocks/text/_ff/text_ff_open-sans.scss';
+import '@blocks/text/_fw/text_fw_bold.scss';
+import '@blocks/text/_mt/text_mt_12.scss';
+
+import '@blocks/contacts/__phone/contacts__phone.scss';
+import '@blocks/contacts/__text/contacts__text.scss';
+
+import '@blocks/skills/skills.scss';
+import '@blocks/skills/__ul/skills__ul.scss';
+import '@blocks/skills/__list-item/skills__list-item.scss';
